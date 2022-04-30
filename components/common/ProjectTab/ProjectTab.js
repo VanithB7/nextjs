d2d09b12f6e8e17e266f112e912a9f4e64edbe73
VanithB <vanith@doodleblue.com>
2022-05-01 04:10:37 +0530

    basesetup#2

--- components/common/ProjectTab/ProjectTab.js
+++ components/common/ProjectTab/ProjectTab.js
@@ -4,10 +4,10 @@ const ProjectTab=({projectTabData})=>{
      <div className="bg-white-500 shadow  mb-5 w-full lg:w-1/2  m-auto">
           <ul className="flex justify-around  h-24 items-center">
 
-             {projectTabData.map((projecttabdata)=>{
+             {projectTabData.map((projecttabdata,index)=>{
 
                  return(
-                      <li className="font-bold text-1xl lg:text-2xl ">
+                      <li key={index} className="font-bold text-1xl lg:text-2xl ">
                   
                   <Link  href={projecttabdata.path}><a className={projecttabdata.active?'text-[#bda588]':'text-[#1f1d5e] hover:text-[#21ace2]'} >{projecttabdata.menuName}</a></Link>
               </li>
