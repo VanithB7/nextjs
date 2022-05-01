d0bac8955c676d97ef9a44c9a00dce228cf299df
VanithB <vanith@doodleblue.com>
2022-05-02 03:50:09 +0530

    basesetup#14

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -22,7 +22,7 @@ const ContactUs =()=>{
          <div  className="lg:col-span-8 xl:col-span-9 mt-5">
             <iframe
             width="100%"
-          height="600"
+        height="100%"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.993628620751!2d77.59575911413513!3d12.972259118375275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1460e51c5111%3A0x7726317a3c7628aa!2sVaishnavi%20Group!5e0!3m2!1sen!2sin!4v1602343331772!5m2!1sen!2sin"></iframe>
         </div>
     </div>
