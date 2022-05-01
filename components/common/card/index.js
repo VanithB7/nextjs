6bf360c244e42f26d6a0b01512dae130e13a990e
VanithB <vanith@doodleblue.com>
2022-05-02 02:47:00 +0530

    basesetup#11

--- components/common/card/index.js
+++ components/common/card/index.js
@@ -8,9 +8,9 @@
    <div className="p-5">
     <h2 className="font-bold text-2xl  text-[#1F1D5E]">{portfolio.projectTitle}</h2>
     <p className="mb-5 mt-4">{portfolio.projectLocation}</p>
-    {portfolio.projectNoOfUnit && (<div className="grid grid-cols-4 border-b-grey-400 mt-2"><p  className="col-span-1">No of units</p><p  className="col-span-3">{portfolio.projectNoOfUnit}</p></div>)}
-    {portfolio.type && (<div className="grid grid-cols-4 mt-2"><p className="col-span-1">Type</p> <p className="col-span-3 break-words ">{portfolio.type}</p></div>)}
-    {portfolio.rera && ( <div  className="grid grid-cols-4 mt-2 "><p  className="col-span-1"> RERA</p><p  className="col-span-3 break-words ">{portfolio.rera}</p> </div>)}
+    {portfolio.projectNoOfUnit && (<div className="grid grid-cols-4 border-b-grey-400 mt-2"><p  className="col-span-2 sm:col-span-1">No of units</p><p  className="col-span-2 sm:col-span-3">{portfolio.projectNoOfUnit}</p></div>)}
+    {portfolio.type && (<div className="grid grid-cols-4 mt-2"><p className="col-span-2 sm:col-span-1">Type</p> <p className="col-span-2 sm:col-span-3 break-words ">{portfolio.type}</p></div>)}
+    {portfolio.rera && ( <div  className="grid grid-cols-4 mt-2 "><p  className="col-span-2 sm:col-span-1"> RERA</p><p  className="col-span-2 sm:col-span-3 break-words ">{portfolio.rera}</p> </div>)}
    </div>
 </div>
     );
